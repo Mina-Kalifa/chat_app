237617d7d2117c5d263fa1e4c94759fc4eac0c14
Mina Khalifa <88984868+Mina-Kalifa@users.noreply.github.com>
2024-02-21 13:29:13 +0200

    Update login_view.dart

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -27,126 +27,131 @@ class _LoginViewState extends State<LoginView> {
           ? Center(
               child: CircularProgressIndicator(),
             )
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Container(
-                    height: 100,
-                    width: 250,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/cat.jpg'),
-                        fit: BoxFit.fill,
+          : Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Container(
+                        height: 100,
+                        width: 250,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/cat.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const Text(
-                  'Chat App',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 28,
-                        ),
+                    const Text(
+                      'Chat App',
+                      style: TextStyle(
+                        fontSize: 30,
                       ),
-                    ],
-                  ),
-                ),
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      CustomTextFormField(
-                        getData: (value) {
-                          email = value;
-                        },
-                        label: 'Email',
-                        hint: 'Enter Your Email',
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 28,
+                            ),
+                          ),
+                        ],
                       ),
-                      CustomTextFormField(
-                        getData: (value) {
-                          password = value;
-                        },
-                        label: 'Password',
-                        hint: 'Enter your password',
-                        isPassword: true,
+                    ),
+                    Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          CustomTextFormField(
+                            getData: (value) {
+                              email = value;
+                            },
+                            label: 'Email',
+                            hint: 'Enter Your Email',
+                          ),
+                          CustomTextFormField(
+                            getData: (value) {
+                              password = value;
+                            },
+                            label: 'Password',
+                            hint: 'Enter your password',
+                            isPassword: true,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                CustomButton(
-                  text: 'Login',
-                  onpressed: () async {
-                    setState(() {
-                      _isLoading = true;
-                    });
-                    if (formKey.currentState!.validate()) {
-                      try {
-                        await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: email, password: password);
-                        Navigator.pushNamed(
-                          context,
-                          ChatView.chatId,
-                          arguments: email,
-                        );
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'user-not-found') {
-                          showSnakeBar(
-                            context,
-                            'No user found for that email.',
-                          );
-                        } else if (e.code == 'wrong-password') {
-                          showSnakeBar(
-                            context,
-                            'Wrong password provided for that user.',
-                          );
-                        }
-                      } catch (e) {
-                        showSnakeBar(
-                          context,
-                          e.toString(),
-                        );
-                      } finally {
+                    ),
+                    CustomButton(
+                      text: 'Login',
+                      onpressed: () async {
                         setState(() {
-                          _isLoading = false;
+                          _isLoading = true;
                         });
-                      }
-                    } else {
-                      setState(() {
-                        _isLoading = false;
-                      });
-                    }
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('don\'t have an account?'),
-                      const SizedBox(
-                        width: 10,
+                        if (formKey.currentState!.validate()) {
+                          try {
+                            await FirebaseAuth.instance
+                                .signInWithEmailAndPassword(
+                                    email: email, password: password);
+                            Navigator.pushNamed(
+                              context,
+                              ChatView.chatId,
+                              arguments: email,
+                            );
+                          } on FirebaseAuthException catch (e) {
+                            if (e.code == 'user-not-found') {
+                              showSnakeBar(
+                                context,
+                                'No user found for that email.',
+                              );
+                            } else if (e.code == 'wrong-password') {
+                              showSnakeBar(
+                                context,
+                                'Wrong password provided for that user.',
+                              );
+                            }
+                          } catch (e) {
+                            showSnakeBar(
+                              context,
+                              e.toString(),
+                            );
+                          } finally {
+                            setState(() {
+                              _isLoading = false;
+                            });
+                          }
+                        } else {
+                          setState(() {
+                            _isLoading = false;
+                          });
+                        }
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('don\'t have an account?'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () => Navigator.pushNamed(
+                                context, RegisterView.registerId),
+                            child: const Text(
+                              'Register',
+                            ),
+                          ),
+                        ],
                       ),
-                      GestureDetector(
-                        onTap: () => Navigator.pushNamed(
-                            context, RegisterView.registerId),
-                        child: const Text(
-                          'Register',
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
     );
   }
